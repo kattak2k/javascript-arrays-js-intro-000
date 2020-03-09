b4c608fe4ea102fea8c5e3c6888ecf2f7c98a1a9
Ravi <kkiran.ror@gmail.com>
2020-03-09 01:39:52 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,16 @@
+chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArray(array, element) {
+   return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+   return [element, ...array]
+}
+
+function addElementToEndOfArray(array, element) {
+   return [...array, element]
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+   return [...array, element]
+}
